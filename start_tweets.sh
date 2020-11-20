e734329961f777a9cc971bc086c7ba439b0d59e1
morgulbrut <tillo.bosshart@gmail.com>
2020-11-20 13:47:05 +0000

    WIP getting autouptdates to work

---
+++ start_tweets.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd /home/pi/Documentos/pytwebot
+STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+while [  $STATE == "error" ]; do
+    #do a ping and check that its not a default message or change to grep for something else
+    STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+
+    #sleep for 2 seconds and try again
+    sleep 2
+ done
+echo "Pulling updates..."
+echo
+git pull
+echo
+echo "Pull done..."
+echo "Initializating tweets.py..."
+
+echo nope | python tweets.py
